68ab89bccdf4c07da96e8c07cab328196fca97b1
Tusharjamdade <tusharnjamdade@gmail.com>
2024-08-23 08:40:30 +0530

    Testing Git

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { AccordionDemo } from "@/components/AccordionDemo";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+export default function Home() {
+  return (
+    <div>
+      FAQ
+      <AccordionDemo/>
+    </div>
+  );
+}
